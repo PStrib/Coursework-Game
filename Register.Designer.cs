d4d89bf57fe027cf8588ac39601f2841c1b182e1
Peter Stribblehill <peter.strib@gmail.com>
2021-03-13 22:18:42 +0000

    Improve Register form spacing

--- Register.Designer.cs
+++ Register.Designer.cs
@@ -69,7 +69,7 @@ namespace Coursework_Game
             // txtUsername
             // 
             this.txtUsername.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsername.Location = new System.Drawing.Point(127, 122);
+            this.txtUsername.Location = new System.Drawing.Point(93, 131);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(312, 27);
             this.txtUsername.TabIndex = 2;
@@ -77,27 +77,27 @@ namespace Coursework_Game
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsername.Location = new System.Drawing.Point(50, 125);
+            this.lblUsername.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(45, 103);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(66, 19);
+            this.lblUsername.Size = new System.Drawing.Size(87, 25);
             this.lblUsername.TabIndex = 3;
             this.lblUsername.Text = "Username:";
             // 
             // lblPassword1
             // 
             this.lblPassword1.AutoSize = true;
-            this.lblPassword1.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPassword1.Location = new System.Drawing.Point(52, 263);
+            this.lblPassword1.Font = new System.Drawing.Font("Bahnschrift Condensed", 16F);
+            this.lblPassword1.Location = new System.Drawing.Point(45, 313);
             this.lblPassword1.Name = "lblPassword1";
-            this.lblPassword1.Size = new System.Drawing.Size(64, 19);
+            this.lblPassword1.Size = new System.Drawing.Size(87, 27);
             this.lblPassword1.TabIndex = 4;
             this.lblPassword1.Text = "Password:";
             // 
             // txtPassword1
             // 
             this.txtPassword1.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword1.Location = new System.Drawing.Point(126, 263);
+            this.txtPassword1.Location = new System.Drawing.Point(93, 343);
             this.txtPassword1.Name = "txtPassword1";
             this.txtPassword1.PasswordChar = '*';
             this.txtPassword1.Size = new System.Drawing.Size(313, 31);
@@ -106,7 +106,7 @@ namespace Coursework_Game
             // txtPassword2
             // 
             this.txtPassword2.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword2.Location = new System.Drawing.Point(128, 311);
+            this.txtPassword2.Location = new System.Drawing.Point(93, 416);
             this.txtPassword2.Name = "txtPassword2";
             this.txtPassword2.PasswordChar = '*';
             this.txtPassword2.Size = new System.Drawing.Size(312, 31);
@@ -115,27 +115,27 @@ namespace Coursework_Game
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 323);
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 16F);
+            this.label1.Location = new System.Drawing.Point(45, 386);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 19);
+            this.label1.Size = new System.Drawing.Size(142, 27);
             this.label1.TabIndex = 6;
             this.label1.Text = "Retype Password:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(50, 174);
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 16F);
+            this.label2.Location = new System.Drawing.Point(45, 170);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 19);
+            this.label2.Size = new System.Drawing.Size(89, 27);
             this.label2.TabIndex = 9;
             this.label2.Text = "Forename:";
             // 
             // txtForename
             // 
             this.txtForename.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtForename.Location = new System.Drawing.Point(127, 171);
+            this.txtForename.Location = new System.Drawing.Point(93, 200);
             this.txtForename.Name = "txtForename";
             this.txtForename.Size = new System.Drawing.Size(312, 27);
             this.txtForename.TabIndex = 8;
@@ -143,17 +143,17 @@ namespace Coursework_Game
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(50, 216);
+            this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 16F);
+            this.label3.Location = new System.Drawing.Point(45, 241);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 19);
+            this.label3.Size = new System.Drawing.Size(82, 27);
             this.label3.TabIndex = 11;
             this.label3.Text = "Surname:";
             // 
             // txtSurname
             // 
             this.txtSurname.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSurname.Location = new System.Drawing.Point(127, 213);
+            this.txtSurname.Location = new System.Drawing.Point(92, 271);
             this.txtSurname.Name = "txtSurname";
             this.txtSurname.Size = new System.Drawing.Size(312, 27);
             this.txtSurname.TabIndex = 10;
@@ -174,18 +174,18 @@ namespace Coursework_Game
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 661);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPassword1);
             this.Controls.Add(this.btnCreateAccount);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtSurname);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtForename);
             this.Controls.Add(this.txtPassword1);
-            this.Controls.Add(this.lblPassword1);
             this.Controls.Add(this.lblUsername);
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.btnBackButton);
             this.Controls.Add(this.txtPassword2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.SplashBackground2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Register";
