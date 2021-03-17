d144092890d1b2ceb691c3231b1c606464a1dee7
Peter Stribblehill <peter.strib@gmail.com>
2021-03-17 18:34:44 +0000

    Make a board of buttons in game.cs

--- Game.cs
+++ Game.cs
@@ -13,11 +13,37 @@ namespace Coursework_Game
     public partial class Game : Form
     {
         private User user;
+        private const int X_ELEMENTS= 3;
+        private const int Y_ELEMENTS = 3;
+        private int[,] gameboard = new int [12, 12];
+
         public Game(User user)
         {
             this.user = user;
             InitializeComponent();
             pboxAvatar.Image = user.Avatar;
+            Button[,] squares = new Button[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    squares[i, j] = new Button();
+                    squares[i, j].Height = 50;
+                    squares[i, j].Width = 50;
+                    int [] tagArray = new int[] { i, j };
+                    squares[i, j].Tag = tagArray;
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    squares[i, j].Location = new Point(i * 51+710, j * 51+250);
+                    this.Controls.Add(squares[i, j]);
+                }
+            }
+            var rand = new Random();
         }
     }
 }
